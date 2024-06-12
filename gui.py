1581b6b0a19f43857e0190275afd4e251b4692f0
Francesco Albano <119073350+Hardin22@users.noreply.github.com>
2024-06-13 00:46:26 +0200

    aggiornamento gui

--- gui.py
+++ gui.py
@@ -1,29 +1,42 @@
 import customtkinter as ctk
 from tkinter import font as tkfont
+from PIL import Image, ImageTk
 from main import start_game
 from chessboard import ChessBoard
 import threading
 
 class ChessApp(ctk.CTk):
-    def __init__(self, update_label_callback, update_eval_bar_callback):
+    def __init__(self, update_label_callback, update_eval_bar_callback, update_best_moves_callback):
         super().__init__()
         self.title("Scacchi")
         self.geometry("600x1000")
-
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("green")
         self.update_label_callback = update_label_callback
         self.update_eval_bar_callback = update_eval_bar_callback
+        self.update_best_moves_callback = update_best_moves_callback
         self.start_page = StartPage(self)
         self.difficulty_page = DifficultyPage(self)
         self.game_frame = ctk.CTkFrame(self)
         self.chessboard = ChessBoard(self.game_frame)
         self.label = ctk.CTkLabel(self.game_frame, text="", bg_color="grey", text_color="black", font=ctk.CTkFont(size=18))
 
+        # Labels per le migliori mosse
+        self.first_move_label = ctk.CTkLabel(self.game_frame, text="", text_color="white", font=ctk.CTkFont(size=18))
+        self.second_move_label = ctk.CTkLabel(self.game_frame, text="", text_color="white", font=ctk.CTkFont(size=18))
+        self.third_move_label = ctk.CTkLabel(self.game_frame, text="", text_color="white", font=ctk.CTkFont(size=18))
+
+        # Pulsanti
         self.toggle_eval_button = ctk.CTkButton(self.game_frame, text="Toggle Eval Bar", command=self.toggle_eval_bar)
+        self.toggle_best_moves_button = ctk.CTkButton(self.game_frame, text="Toggle Best Moves", command=self.toggle_best_moves)
+
         self.eval_bar = ctk.CTkCanvas(self.game_frame, width=512, height=20, bg="white")
         self.show_eval_bar = False
+        self.show_best_moves = False
 
         self.start_page.pack(fill="both", expand=True)
         self.update_gui()
+        self.resizable(False, False)
 
     def show_start_page(self):
         self.difficulty_page.pack_forget()
@@ -40,11 +53,12 @@ class ChessApp(ctk.CTk):
         self.difficulty_page.pack_forget()
         self.start_page.pack_forget()
         self.game_frame.pack(fill="both", expand=True)
-        self.label.pack(side="bottom", fill="x")
+        self.label.pack(fill="x")
         self.toggle_eval_button.pack(side="bottom")
+        self.toggle_best_moves_button.pack(side="bottom")
         self.chessboard.pack(side="top", fill="both", expand=True)
         self.update_label("waiting for first move")
-        threading.Thread(target=start_game, args=(self.update_label_callback, self.update_eval_bar, difficulty, self.chessboard)).start()
+        threading.Thread(target=start_game, args=(self.update_label_callback, self.update_eval_bar, difficulty, self.chessboard, self.update_best_moves_callback)).start()
 
     def update_label(self, text):
         self.label.configure(text=text)
@@ -58,6 +72,17 @@ class ChessApp(ctk.CTk):
         else:
             self.eval_bar.place_forget()
 
+    def toggle_best_moves(self):
+        self.show_best_moves = not self.show_best_moves
+        if self.show_best_moves:
+            self.first_move_label.pack()
+            self.second_move_label.pack()
+            self.third_move_label.pack()
+        else:
+            self.first_move_label.pack_forget()
+            self.second_move_label.pack_forget()
+            self.third_move_label.pack_forget()
+
     def update_eval_bar(self, eval_score):
         if not self.show_eval_bar:
             return
@@ -66,28 +91,58 @@ class ChessApp(ctk.CTk):
         if eval_score > 0:
             eval_width = min(512, middle_x * eval_score / 10)
             self.eval_bar.create_rectangle(middle_x - eval_width, 0, middle_x, 20, fill="white",
-                                                   outline="grey", tags="eval")
+                                           outline="grey", tags="eval")
         else:
             eval_width = min(512, middle_x * abs(eval_score) / 10)
             self.eval_bar.create_rectangle(middle_x, 0, middle_x + eval_width, 20, fill="black",
-                                                   outline="grey", tags="eval")
+                                           outline="grey", tags="eval")
+
+    def update_best_moves(self, moves):
+        if not self.show_best_moves:
+            return
+        self.first_move_label.configure(text=moves[0] if len(moves) > 0 else "")
+        self.second_move_label.configure(text=moves[1] if len(moves) > 1 else "")
+        self.third_move_label.configure(text=moves[2] if len(moves) > 2 else "")
 
     def update_gui(self):
         self.after(100, self.update_gui)
+
 
 class StartPage(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         self.configure(bg_color="white")
-        button = ctk.CTkButton(self, text="Start Game", fg_color="red", text_color="black", font=ctk.CTkFont(size=18),
-                               command=master.show_difficulty_page)
-        button.pack(pady=200)
+
+        # Carica l'immagine
+        img = Image.open("images/chesslogo.png")
+        img = img.resize((180, 180), Image.LANCZOS)
+        self.image = ImageTk.PhotoImage(img)
+
+        # Crea un CTkLabel con l'immagine come sfondo
+        image_label = ctk.CTkLabel(self, image=self.image, text="")
+
+        # Posiziona l'immagine in alto al centro
+        image_label.pack(side="top", pady=20)
+
+        pvc = ctk.CTkButton(self, text="Player vs computer", text_color="white", font=ctk.CTkFont(size=14),
+                            command=master.show_difficulty_page, width=300)
+        pvc.pack(pady=20)  # Aggiungi fill="x" per espandere il pulsante orizzontalmente
+
+        pvp = ctk.CTkButton(self, text="Player vs Player", text_color="white", font=ctk.CTkFont(size=14),
+                            command=master.show_difficulty_page, width=300)
+        pvp.pack(pady=20)  # Aggiungi fill="x" per espandere il pulsante orizzontalmente
+
 
 class DifficultyPage(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         self.configure(bg_color="grey")
 
+        self.title = ctk.CTkLabel(self, text="Select difficulty", font=ctk.CTkFont(size=20), text_color="white")
+        self.title.pack(pady=20)
+
+        self.goback = ctk.CTkButton(self, text="Go back", text_color="white", font=ctk.CTkFont(size=14), command=master.show_start_page)
+        self.goback.pack(pady=20)
         self.buttons = [
             self.create_button("Easy", 1),
             self.create_button("Medium", 10),
@@ -128,12 +183,17 @@ class DifficultyPage(ctk.CTkFrame):
         self.buttons[self.current_button_index].invoke()
         return "break"
 
+
 def update_label(text):
     app.update_label(text)
+
 
 def update_eval_bar(eval_score):
     app.update_eval_bar(eval_score)
 
+def update_best_moves(moves):
+    app.update_best_moves(moves)
+
 if __name__ == "__main__":
-    app = ChessApp(update_label, update_eval_bar)
+    app = ChessApp(update_label, update_eval_bar, update_best_moves)
     app.mainloop()
--- main.py
+++ main.py
@@ -7,19 +7,23 @@ engine = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
 
 board = chess.Board()
 
+
 def best_move(board):
     result = engine.play(board, chess.engine.Limit(time=2.0))
     return result.move
+
 
 def best_moves(board, num_moves=3):
     result = engine.analyse(board, chess.engine.Limit(time=2.0), multipv=num_moves)
     return [(info['pv'][0], info['score'].relative) for info in result]
 
+
 def evaluate_position(board):
     info = engine.analyse(board, chess.engine.Limit(time=0.1))
     return info["score"].relative.score(mate_score=10000) / 100.0
 
-def start_game(update_label_callback, update_eval_bar_callback, difficulty, chessboard):
+
+def start_game(update_label_callback, update_eval_bar_callback, difficulty, chessboard, update_best_moves_callback):
     engine.configure({"Skill Level": difficulty})
 
     def game_loop():
@@ -31,11 +35,14 @@ def start_game(update_label_callback, update_eval_bar_callback, difficulty, ches
             print()
             print()
             moves = best_moves(board)
+            best_moves_san = [board.san(move) for move, score in moves]  # Converti le migliori mosse in SAN
+            update_best_moves_callback(best_moves_san)  # Passa le migliori mosse alla funzione di callback
             for i, (move, score) in enumerate(moves):
                 print(f"Top {i + 1} move: {board.san(move)} with score {score}")
             print()
             user_move = input("Enter your move: ")
-            game_over = make_move(user_move, update_label_callback, update_eval_bar_callback, chessboard)
+            game_over = make_move(user_move, update_label_callback, update_eval_bar_callback, chessboard,
+                                  update_best_moves_callback)
 
         engine.quit()
 
@@ -43,15 +50,14 @@ def start_game(update_label_callback, update_eval_bar_callback, difficulty, ches
     game_thread.start()
 
 
-
-def make_move(move, update_label_callback, update_eval_bar_callback, chessboard):
+def make_move(move, update_label_callback, update_eval_bar_callback, chessboard, update_best_moves_callback=None):
     try:
         board.push_san(move)
         print(f"Player move: {move}")
         update_label_callback(f"Player move: {move}")
         chessboard.update_board(board.fen())
         eval_score = evaluate_position(board)
-        print(f"Player evaluation: {eval_score}")  # Aggiungi questa riga
+        print(f"Player evaluation: {eval_score}")
         update_eval_bar_callback(eval_score)
         if board.is_game_over():
             print("Game over")
@@ -63,14 +69,22 @@ def make_move(move, update_label_callback, update_eval_bar_callback, chessboard)
         update_label_callback(f"Engine move: {response}")
         chessboard.update_board(board.fen())
         eval_score = evaluate_position(board)
-        print(f"Engine evaluation: {eval_score}")  # Aggiungi questa riga
+        print(f"Engine evaluation: {eval_score}")
         update_eval_bar_callback(eval_score)
-        update_label_callback(eval_score)
+
+        # Aggiorna le migliori mosse per il prossimo turno
+        if update_best_moves_callback:
+            moves = best_moves(board)
+            best_moves_san = [board.san(move) for move, score in moves]
+            update_best_moves_callback(best_moves_san)
+
     except ValueError:
         print("Invalid move. Try again.")
         update_label_callback("Invalid move. Try again.")
     return False
 
+
 if __name__ == "__main__":
     from gui import start_gui
-    start_gui(lambda x: print(x), lambda x: print(f"Eval: {x}"), 1, ChessBoard())
+
+    start_gui(lambda x: print(x), lambda x: print(f"Eval: {x}"), lambda x: print(f"Best moves: {x}"), 1, ChessBoard())
